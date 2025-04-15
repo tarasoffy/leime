db16d66afa6774623d68f048741cd2891df8b138
Maksym Yaremenko <maxtarasoff9@gmail.com>
2025-04-15 12:05:59 +0300

    add key

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -4,7 +4,7 @@ import { mems } from "@/mock/mems";
 export default function ListPage() {
   return (
     <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-      {mems.map(item => <CardMem mem={item}/>)}
+      {mems.map(item => <CardMem key={item.id} mem={item}/>)}
     </section>
   );
 }
